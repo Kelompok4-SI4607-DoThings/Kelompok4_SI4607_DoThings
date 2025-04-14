5b1d4a4a47d42ed487bad319899255bd84e06906
mikhabigail <mikhanatannoah@gmail.com>
2025-04-14 20:36:40 +0700

    DOTHINGS-13<mengedit register mikha>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
     <div class="card p-4 shadow rounded" style="width: 100%; max-width: 420px;">
-        <h4 class="text-center mb-4">Create an account</h4>
+        <h4 class="text-center mb-4">Create an Account</h4>
         
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <!-- Nama -->
             <div class="mb-3">
-                <input type="text" name="name" class="form-control" placeholder="Full Name" required value="{{ old('name') }}">
+                <input type="text" name="name" class="form-control" placeholder="Full Name:" required value="{{ old('name') }}">
                 @error('name')
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -18,7 +18,7 @@
 
             <!-- Email -->
             <div class="mb-3">
-                <input type="email" name="email" class="form-control" placeholder="Email" required value="{{ old('email') }}">
+                <input type="email" name="email" class="form-control" placeholder="Email:" required value="{{ old('email') }}">
                 @error('email')
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -26,7 +26,7 @@
 
             <!-- Password -->
             <div class="mb-3">
-                <input type="password" name="password" class="form-control" placeholder="Enter your password" required>
+                <input type="password" name="password" class="form-control" placeholder="Enter your password:" required>
                 @error('password')
                     <small class="text-danger">{{ $message }}</small>
                 @enderror
@@ -34,7 +34,7 @@
 
             <!-- Konfirmasi Password -->
             <div class="mb-3">
-                <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm your password" required>
+                <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm your password:" required>
             </div>
 
             <!-- Role -->
@@ -49,12 +49,12 @@
                 @enderror
             </div>
 
-            <button type="submit" class="btn btn-primary w-100">Create account</button>
+            <button type="submit" class="btn btn-primary w-100">Create Account</button>
         </form>
 
 
         <div class="text-center mt-3">
-            Belum punya akun? <a href="{{ route('login') }}">Log in</a>
+            Already have an account? <a href="{{ route('login') }}">Log In Here!</a>
         </div>
     </div>
 </div>
